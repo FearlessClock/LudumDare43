81960033a7619f6a2eb98986387524250fa3c18c
JackAlgera <jack.algera@live.com>
2018-12-01 18:24:39 +0100

    Changed animation start time to random

---
+++ LDJam43/Assets/Scripts/AnimationStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStart : MonoBehaviour {
+
+    private Animator anim;
+    public int animIdToPlay; 
+
+	void Start () {
+        anim = transform.GetComponent<Animator>();
+        anim.Play(animIdToPlay, 0, Random.Range(0, 1f));
+	}
+}
