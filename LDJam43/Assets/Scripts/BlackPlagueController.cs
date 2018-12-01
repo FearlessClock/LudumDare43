c9f5ff5cb0212b4d448b3264beb5e63e52cf17f4
Pieter Algera <pieteralgera@ymail.com>
2018-12-01 20:09:07 +0100

    Added black plague that spawns randomly and walks across the screen

---
+++ LDJam43/Assets/Scripts/BlackPlagueController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackPlagueController : MonoBehaviour {
+    public Vector3 startingPosition;
+    public Vector3 endPosition;
+    private Vector3 direction;
+    private ParticleSystem smokeParticleSys;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        direction = endPosition - startingPosition;
+        direction.Normalize();
+        this.transform.position = startingPosition;
+        smokeParticleSys = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += direction * speed * Time.deltaTime;
+        if(Vector3.Distance(this.transform.position, endPosition) < 1)
+        {
+            speed = 0;
+            smokeParticleSys.Play();
+            Invoke("RemoveSpriteRenderer", 0.7f);
+            Destroy(this.gameObject, 3);
+            //TODO: Remove population = to about 30%
+        }
+	}
+
+    public void RemoveSpriteRenderer()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(startingPosition, 1f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(endPosition, 1f);
+    }
+}
---
+++ LDJam43/Assets/Scripts/godController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum eGodAngerLevel { Happy, notImpressed, angry, furious}
+public class godController : MonoBehaviour {
+    public float favorLevel;
+
+    public float happyLevel;
+    public float notImpressedLevel;
+    public float angryLevel;
+    public float furiousLevel;
+    public GameObject blackDeathPrefab;
+    public float spawnRange;
+
+    public eGodAngerLevel currentGodAngerLevel;
+
+    public float randomChanceToDoSomething;
+    public float timeTillNextEvent;
+    private float timer;
+	// Use this for initialization
+	void Start () {
+        timer = timeTillNextEvent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(favorLevel <= furiousLevel)
+        {
+            currentGodAngerLevel = eGodAngerLevel.furious;
+        }
+        else if (favorLevel <= angryLevel)
+        {
+            currentGodAngerLevel = eGodAngerLevel.angry;
+        }
+        else if (favorLevel <= notImpressedLevel)
+        {
+            currentGodAngerLevel = eGodAngerLevel.notImpressed;
+        }
+        else if (favorLevel <= happyLevel)
+        {
+            currentGodAngerLevel = eGodAngerLevel.Happy;
+        }
+        timer -= Time.deltaTime;
+        if(timer <= 0 && Random.Range(0f, 1f) < randomChanceToDoSomething)
+        {
+            timer = timeTillNextEvent;
+            switch (currentGodAngerLevel)
+            {
+                case eGodAngerLevel.Happy:
+                    //TODO: Sing a nice song
+                    break;
+                case eGodAngerLevel.notImpressed:
+                    //TODO: Set a building on fire
+                    break;
+                case eGodAngerLevel.angry:
+                    //TODO: Send great wind storm
+                    break;
+                case eGodAngerLevel.furious:
+                    //Create the black plague
+                    GameObject bpObj = Instantiate<GameObject>(blackDeathPrefab, this.transform);
+                    BlackPlagueController bpController = bpObj.GetComponent<BlackPlagueController>();
+                    Vector2 cameraPos = Camera.main.transform.position;
+                    //Start the black plague offscreen and get the point across from the screen
+                    bpController.startingPosition = cameraPos + (Random.insideUnitCircle.normalized * spawnRange);
+                    Vector3 toCenter = (Camera.main.transform.position - bpController.startingPosition).normalized * spawnRange; 
+                    bpController.endPosition = toCenter;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(Camera.main.transform.position, spawnRange);
+    }
+}
