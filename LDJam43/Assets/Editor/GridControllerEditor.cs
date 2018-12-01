577ed6e6d6cdde270b2df995c32743ed7705baf5
Pieter Algera <pieteralgera@ymail.com>
2018-12-01 09:32:28 +0100

    Added grid system

---
+++ LDJam43/Assets/Editor/GridControllerEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(GridController))]
+[CanEditMultipleObjects]
+public class GridControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        GridController gc = (GridController)target;
+    }
+}
---
+++ LDJam43/Assets/Scripts/GridController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour {
+    public GameObject[,] grid;
+    public GameObject floorSprite;
+
+    public Vector2 gridSize;
+    // Use this for initialization
+	void Start () {
+        //Generate the grid with basic tile 
+        grid = new GameObject[(int)gridSize.x, (int)gridSize.y];
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                grid[i, j] = Instantiate<GameObject>(floorSprite, new Vector3(i, j, 0), Quaternion.identity);
+                grid[i, j].transform.parent = this.transform;
+            }
+        }
+	}
+
+    public GameObject GetTileAt(Vector2 pos)
+    {
+        return grid[(int)pos.x, (int)pos.y];
+    }
+
+    public void SetTileAt(Vector2 pos, GameObject tile)
+    {
+        grid[(int)pos.x, (int)pos.y] = tile;
+    }
+}
