ae1cf2eea788111ea5c13a0d89f888e7186b1f80
Pieter Algera <pieteralgera@ymail.com>
2018-12-02 16:28:20 +0100

    Added Text bubble UI

---
+++ LDJam43/Assets/Scripts/TextBubbleController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextBubbleController : MonoBehaviour {
+    public string[] texts;
+    public int currentText;
+    private Coroutine typingCoroutine;
+    public TextMeshProUGUI bubbleUI;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("TypeText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NextTextBubble()
+    {
+        currentText++;
+        if(currentText < texts.Length)
+        {
+            if(typingCoroutine != null)
+            {
+                StopCoroutine(typingCoroutine);
+                typingCoroutine = null;
+            }
+            bubbleUI.text = "";
+            typingCoroutine = StartCoroutine("TypeText");
+        }
+    }
+
+    public IEnumerator TypeText()
+    {
+        bubbleUI.text = "";
+        for (int i = 0; i < texts[currentText].Length; i++)
+        {
+            bubbleUI.text += texts[currentText][i].ToString();
+            yield return 1;
+        }
+    }
+}
